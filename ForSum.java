72d03d63f6ecc301b1cf638fcd1aeec05ff6dac1
Mishon Das <mishon.da@uttarauniversity.edu.bd>
2025-02-21 22:36:19 +0600

    Create ForSum.java

---
+++ ForSum.java
@@ -0,0 +1,13 @@
+public class ForSum {
+    public static void main(String[] args) {
+      int i=0;
+      int sum=0;
+      while(i<=10){
+          sum+=i;
+          i++;
+      }
+      
+      System.out.println("Sum of 1 to 10 number is:"+ sum);
+    }
+    
+}
