188c339e7850be207f5e6a4f2f9b67bbd93c4a91
Mishon Das <mishon.da@uttarauniversity.edu.bd>
2025-02-21 22:25:39 +0600

    Create DoWhileLoof.java

---
+++ DoWhileLoof.java
@@ -0,0 +1,11 @@
+public class DoWhileLoof {
+    public static void main(String[] args) {
+        int i=1;
+        do{
+            System.out.println(i+ ".Mishon Das");
+            i++;
+        }
+        while(i<=10);
+    }
+    
+}
